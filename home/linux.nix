0f977c79d1317d1fbd071e8742a93a9a04bc8d79
tajima2019 <tajima.tajiken@gmail.com>
2026-07-31 17:21:54 +0900

    リファクタ

--- home/linux.nix
+++ home/linux.nix
@@ -1,14 +1,10 @@
 { config, pkgs, username, ... }:
-let
-  repo = "${config.home.homeDirectory}/nix-config";
-  link = path: config.lib.file.mkOutOfStoreSymlink "${repo}/config/${path}";
-in
 {
   home.homeDirectory = "/home/${username}";
 
   targets.genericLinux.enable = true; # 非NixOS Linux で必須
 
   xdg.configFile = {
-    "ghostty/local".source = ../config/ghostty/local_arch;
+    "ghostty/local".source = ../config/ghostty/local_linux;
   };
 }
